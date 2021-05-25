eacad0a434e132f0fd21ecc955970f4cbcd039df
maylargit <may.larmie@gmail.com>
2021-05-25 11:39:06 +0100

    new check

---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+        <h4>Home</h4>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sed aliquam sem, in aliquam nunc. Quisque mollis ac magna ut laoreet. Quisque ut lorem tincidunt urna sodales cursus eu eget velit. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Quisque eu est eros. In sem quam, scelerisque sed dolor sed, placerat consequat nulla. Nulla facilisi. Maecenas eleifend consequat nulla, et egestas turpis sollicitudin vel</p>
+        </div>
+    )
+}
+
+export default Home
---
+++ src/components/Navbar.js
@@ -0,0 +1,17 @@
+ import React from 'react';
+
+ const Navbar = () => {
+     return (
+        <div>
+            <a href="">  'GodFather' Software Developers</a>
+                <ul>
+                    <li><a href=""></a>Home</li>
+                    <li><a href="/about"></a>About</li>
+                    <li><a href="/contact"></a>Contact</li>
+                    <li><a href="/blog"></a>Blog</li>
+                </ul>
+        </div>
+     )
+ }
+
+ export default Navbar;
